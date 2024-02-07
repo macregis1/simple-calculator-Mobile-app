05b54e14e2e4a6a2d667c23001296b9843d6c796
NDAHIRO N Regis <rndahiro552@gmail.com>
2024-02-07 13:23:49 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [input,setInput]= useState('')
+  const [result,setResult] = useState('')
+  const onButtonPress = (value) =>{
+    if(value === '='){
+      try{
+        setResult(eval(input));
+      } catch(error){
+        setResult('error')
+      }
+    }else if(value === 'C') {
+      setInput('');
+      setResult('')
+    }else if(value === '⌫') {
+      setInput(input.slice(0, -1))
+    }else {
+      setInput(input + value)
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.resultContainer}>
+        <Text style={styles.resultText}>
+          {result}
+        </Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+        style={styles.inputText}
+        value={input}
+        onChangeText={setInput}
+        keyboardType='numeric'
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        {['C','%','⌫','/','7','8','9','*','4','5','6','-','1','2','3','+','00','0','.','='].map(
+          (item,index) =>(
+            <TouchableOpacity
+            key={index}
+            style={styles.button}
+            onPress={() => onButtonPress(item)}
+            >
+              <Text style={styles.buttonText}>{item}</Text>
+            </TouchableOpacity>
+          )
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#b2b2b2"
+  }, resultContainer: {
+    flex: 7,
     justifyContent: 'center',
-  },
+    alignItems: 'flex-end'
+  }, resultText: {
+    fontSize: 40,
+  }, inputContainer: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  }, inputText: {
+    fontSize: 30
+  }, buttonContainer: {
+    flex: 7,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    
+  }, button: {
+    width: "21%",
+    // // height: "15%",
+    // width: 80,
+    height: 69,
+    borderRadius: 15,
+    margin: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor:"#ccc",
+  }, buttonText: {
+    fontSize: 24,
+    color: "#000"
+  }
 });
---
+++ ReadMe.txt
@@ -0,0 +1,19 @@
+this is a simple mobile calculator with simple arithmetic operations. 
+it can be upgraded by adding other operations in the future.
+while developing this application several react native components were used, 
+here are those components:
+    . StatusBar from 'expo-status-bar': is used to control the appearance of
+        status bar at the top of the screen.
+    . SafeAreaView is used to ensure that the content of the app is visible and 
+        safe from overlapping with device-specific elements suchas nothes
+    . Text from 'react-native': is used to display the text on the screen.
+    . TextInput is used to capture useer input.
+    . TouchableOpacity is used to create touchable buttons with a visual feedback effect when pressed.
+    . useState is used to mange state in functional components.
+    . StyleSheet is used to create styles for components, improving performance by optimizingthe styles.
+        OR StyleSheet is used to define the styles for respective containers. the styles are then aplied to
+        the components to achieve the desired layout and appearance.
+    . View component is a fundamental building block for creating the layout structure of the app
+
+in short, these features/components used in this application helped in making the functionality of the application,
+and in making the appearance structure of the application.
